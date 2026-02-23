bd4cd6e3b0a041ab1db7fa1f0ee39d4883921f5d
Veljko Djokic <veljko.djokic.ssfon@gmail.com>
2026-02-23 22:32:57 +0100

    Gotova prva sekcija

---
+++ TravelApp.Domain/Entities/Villa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelApp.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+
+        public double Price { get; set; }   
+
+        public int SquareMeters { get; set; }
+        public int Occupancy { get; set; } //zauzetost
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
