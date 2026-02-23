f6c81cfa93ac4bca6477694dc330a79f528ac97b
Veljko Djokic <veljko.djokic.ssfon@gmail.com>
2026-02-23 23:11:13 +0100

    Dodata pocetni izgled stranice za dodavanje villa

---
+++ TravelApp/Controllers/VillaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelApp.Infrastructure.Data;
+
+namespace TravelApp.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public VillaController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            var villas= applicationDbContext.Villas.ToList();
+            return View(villas);
+        }
+    }
+}
